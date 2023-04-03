31f3287a08517c31e0676c6e24106af050e218af
Davit Papoyan <109224121+Papoyan077@users.noreply.github.com>
2023-04-03 15:56:33 +0400

    login form

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,51 +1,68 @@
 import * as React from 'react';
-import {Link} from 'react-router-dom';
 import {LockOutlined, UserOutlined} from '@ant-design/icons';
-import {Button,  Form, Input} from 'antd';
+import {Button, Form, Input} from 'antd';
+
 function Login() {
-    return (
-        <div style={{display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column",width:"100vw",height:"100vh"}}>
-            <div>
-                <img src="#" alt="Logo" />
-            </div>
-            <div style={{backgroundColor:"white", width:"30vw"}} className="border-blue">
-                <div style={{margin:"5%"}}>
-                    <Form
-                        name="normal_login"
-                        className="login-form"
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);}
+        return (
+            <div style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                width: "100vw",
+                height: "100vh"
+            }}>
+                <div>
+                    <img src="#" alt="Logo"/>
+                </div>
+
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Username!',
+                            },
+                        ]}
                     >
-                        <Form.Item
-                            name="username"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your Username!',
-                                },
-                            ]}
-                        >
-                            <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Username"/>
-                        </Form.Item>
-                        <Form.Item
-                            name="password"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your Password!',
-                                },
-                            ]}
-                        >
-                            <Input
-                                prefix={<LockOutlined className="site-form-item-icon"/>}
-                                type="password"
-                                placeholder="Password"
-                            />
-                        </Form.Item>
-                        <Form.Item style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-                            <Link to={"/general/employees"}> <Button type="dashed" htmlType="submit" className="login-form-button">
-                                Log in
-                            </Button></Link>
-                        </Form.Item>
-                    </Form></div></div></div>
-    )
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Password!',
+                            },
+                        ]}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+                        </Button>
+                    </Form.Item>
+                </Form>
+
+            </div>
+        )
+
 }
+
 export default Login
