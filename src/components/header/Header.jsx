94156adbdbc68b40f24748f32f23e26d3a471c82
Davit Papoyan <109224121+Papoyan077@users.noreply.github.com>
2023-04-06 14:51:46 +0400

    sfsfdf

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ import {LogoutOutlined} from "@ant-design/icons";
 export default function Header() {
     return (
         <div style={{width: "100%", height: "60px"}}>
-            <div className="border-blue" style={{display:"flex" , justifyContent:"space-between" , alignItems:"center" , padding: "0 10px" , width:"100%" }}>
+            <div className="border-table" style={{display:"flex" , justifyContent:"space-between" , alignItems:"center" , padding: "0 10px" , width:"100%" }}>
                 <div>INSTIGATE MOBILE</div>
                 <div style={{display: "flex" , height: "50px",alignItems: "center"}}>
                     <Link style={{marginLeft: "10px",width:"100%",listStyle:"none"}} to='/'>
--- src/components/main/employees/EmpDetail.jsx
+++ src/components/main/employees/EmpDetail.jsx
@@ -1,28 +1,24 @@
 import {useEffect, useState} from "react";
 import {Link, useParams} from "react-router-dom";
 import {getEmployeesById} from "../../../utils/Route";
-import {ArrowLeftOutlined, EditOutlined, UserOutlined} from "@ant-design/icons";
+import {ArrowLeftOutlined, UserOutlined} from "@ant-design/icons";
 import {Table} from "antd";
-import EmpPermissionEdit from "./EmpPermissionEdit";
-import EmpRoleEdit from "./EmpRoleEdit";
 
 const EmpDetail = () => {
     let empId = useParams();
     const [empData, empDataChange] = useState({});
-
     useEffect(() => {
         getEmployeesById(empDataChange, empId)
     }, [empId]);
-
-    const dataSource = empData.permissions
-
+    console.log(empData)
+    let rolesData = empData.roles
+    console.log(rolesData)
     const columns = [
         {
             title: 'Title',
             dataIndex: 'title',
-            key: 'title',
+            key: 'name',
         },]
-    console.log(empData);
     return (
         <div style={{
             display: "flex",
@@ -39,7 +35,7 @@ const EmpDetail = () => {
                     marginLeft: "10px",
                     display: "flex",
                     alignItems: "center"
-                }}>Employee :<UserOutlined style={{marginLeft:"15px"}}/></span>
+                }}>Employee :<UserOutlined style={{marginLeft: "15px"}}/></span>
                 {empData &&
                     <div>
                         <div style={{display: "flex"}}>
@@ -50,46 +46,85 @@ const EmpDetail = () => {
                     </div>
                 }
             </div>
-            <div className="border-blue" style={{width: "100%", height: "100%", overflow: "scroll"}}>
-                <div style={{display: "flex", justifyContent:"space-around", margin: "10px"}}>
-
-                    <div style={{
-                        marginLeft: "25px",
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "center"
-                    }}>
-                        <span style={{display:"flex",alignItems:"center",justifyContent:"center", width: "100%", fontSize: "20px"}}>
+            <div className="border-table" style={{width: "100%", height: "100%", overflow: "scroll"}}>
+                <div style={{
+                    marginLeft: "25px",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center"
+                }}>
+                        <span style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            width: "100%",
+                            fontSize: "20px"
+                        }}>
                             Permissions
-                            <EmpPermissionEdit/>
                         </span>
-                        <div style={{
-                            width: "30vw",
-                            maxHeight: "50vh",
-                            overflow: "scroll"
-                        }}>
-                            <div style={{paddingLeft: "25px"}}></div>
-                            <Table pagination={false} dataSource={dataSource} columns={columns}/>
-                        </div>
-                    </div>
                     <div style={{
-                        marginLeft: "25px",
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "center"
+                        width: "100%",
+                        maxHeight: "70vh",
+                        overflow: "scroll"
                     }}>
-                        <span style={{display:"flex",alignItems:"center",justifyContent:"center", width: "100%", fontSize: "20px"}}>
-                            Roles
-                            <EmpRoleEdit/>
-                        </span>
-                        <div style={{
-                            width: "30vw",
-                            maxHeight: "50vh",
-                            overflow: "scroll"
+                        <Table
+                            columns={columns}
+                            expandable={{
+                                expandedRowRender: (record) => (
+                                    <p
+                                        style={{
+                                            margin: 0,
+                                        }}
+                                    >
+                                        {record.id}
+                                    </p>
+                                ),
+                                rowExpandable: (record) => record.id !== 'Not Expandable',
+                            }}
+                            dataSource={empData.permissions}
+                        />
+                    </div>
+                </div>
+                <div style={{
+                    marginLeft: "25px",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center"
+                }}>
+                        <span style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            width: "100%",
+                            fontSize: "20px"
                         }}>
-                            <div style={{paddingLeft: "25px"}}></div>
-                            <Table pagination={false} dataSource={dataSource} columns={columns}/>
-                        </div>
+                            Roles
+                        </span>
+                    <div style={{
+                        width: "100%",
+                        maxHeight: "70vh",
+                        overflow: "scroll"
+                    }}>
+                        <Table
+                            columns={columns}
+                            expandable={{
+                                expandedRowRender: (record) => (
+                                    <p
+                                        style={{
+                                            margin: 0,
+                                        }}
+                                    >
+                                        {
+                                            record.permissions.map((e)=>{
+                                                return <span>{e.title},   </span>
+                                            })
+                                        }
+                                    </p>
+                                ),
+                                rowExpandable: (record) => record.id !== 'Not Expandable',
+                            }}
+                            dataSource={rolesData}
+                        />
                     </div>
                 </div>
             </div>
