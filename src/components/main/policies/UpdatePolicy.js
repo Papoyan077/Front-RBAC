2916443c423e1f8f23beedde9e3b87ceb84c98e4
Davit Papoyan <109224121+Papoyan077@users.noreply.github.com>
2023-04-18 15:57:45 +0400

    policy update

--- src/components/main/policies/UpdatePolicy.js
+++ src/components/main/policies/UpdatePolicy.js
@@ -3,9 +3,10 @@ import {useEffect, useState} from 'react';
 import instance from '../../../utils/axios';
 import {EditOutlined} from '@ant-design/icons';
 import {Checkbox} from 'antd';
+
 const UpdatePolicy = ({render, setRender, id, moduleTitle, record}) => {
     const [open, setOpen] = useState(false);
-    const [permissions , setPermissions] = useState([]);
+    const [actions, setActions] = useState([]);
     const [actionData, actionDataChange] = useState([]);
     let lastIndex = 0
     const updateIndex = () => {
@@ -20,48 +21,59 @@ const UpdatePolicy = ({render, setRender, id, moduleTitle, record}) => {
             console.log(err.message);
         })
     }, [render]);
-
     const onChange = (checkedValues) => {
-        if(checkedValues.target.checked){
-            permissions.push(checkedValues.target.value)
-        }else {
-            permissions.pop(checkedValues.target.value)
+        if (checkedValues.target.checked) {
+            actions.push(checkedValues.target.value)
+            console.log(actions)
+        } else {
+            actions.pop(checkedValues.target.value)
+            console.log(actions)
         }
+
     };
 
     const UpdatePolicies = async () => {
-        await instance.put(`/policy/${id}`, {actions:permissions, status: "published"})
+        await instance.put(`/policy/${id}`, {actions: actions, status: "published"})
             .then(resp => {
                 setRender(!render)
+                setActions([])
             }).catch((err) => {
                 console.log(err.message);
             });
         setOpen(false);
     };
-
     const ids = record.actions.map((m) => {
-        permissions.push(m.id)
-            return m.id
+        actions.push(m.id)
+        return m.id
     });
 
     return (
         <>
-            <EditOutlined onClick={() => {setOpen(true)}} />
+            <EditOutlined onClick={() => {
+                setOpen(true)
+                setActions([])
+            }}/>
             <Modal
                 title="Update Policy"
                 centered
                 open={open}
                 onOk={() => {
                     UpdatePolicies();
-                    setPermissions([]);
+                    setActions([]);
                 }}
-                onCancel={() => setOpen(false)}
+                onCancel={() => {
+                    setOpen(false)
+                    setActions([]);
+                }
+
+                }
             >
-                <div style={{display:"flex",flexDirection:"column"}}>
+                <div style={{display: "flex", flexDirection: "column"}}>
                     <span style={{fontSize: "20px"}}>Module Name: {moduleTitle}</span>
-                    <div  style={{marginTop:"5%"}}>
-                        Select Actions :  {actionData.map((action)=>
-                        <Checkbox onChange={onChange} defaultChecked={ids.includes(action.id)} key={`action${updateIndex()}`}  value={action.id}>{action.title}</Checkbox>
+                    <div style={{marginTop: "5%"}}>
+                        Select Actions : {actionData.map((action) =>
+                        <Checkbox onChange={onChange} defaultChecked={ids.includes(action.id)}
+                                  key={`action${updateIndex()}`} value={action.id}>{action.title}</Checkbox>
                     )}</div>
                 </div>
             </Modal>
